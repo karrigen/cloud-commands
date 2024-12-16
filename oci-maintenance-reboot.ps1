faf181d525b33336ffcbcbd5d7768154ee3549c5
zhou dai <zhou.dai@mitratech.com>
2024-12-16 10:52:31 +1030

    oci-maitenance-reboot

---
+++ oci-maintenance-reboot.ps1
@@ -0,0 +1,5 @@
+oci search resource structured-search `
+    --query-text "QUERY instance resources return region, timeMaintenanceRebootDue where timeMaintenanceRebootDue > 'Now' sorted by timeMaintenanceRebootDue desc" `
+    --query 'data.items[].{name:"display-name",state:"lifecycle-state",RebootTime:"additional-details"."timeMaintenanceRebootDue", region:"additional-details"."region"}' `
+    --region 'syd' `
+    --output table
