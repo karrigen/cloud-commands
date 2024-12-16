fcee7684ab7098e899b7cbe9554259c6397ba3d0
zhou dai <zhou.dai@mitratech.com>
2024-12-16 11:02:57 +1030

    oci-maitenance-reboot

---
+++ oci-maintenance-reboot-regions.ps1
@@ -0,0 +1,41 @@
+# Showing maintenance reboot time for all VMs in all compartments in all regions
+# The script runs in Powershell with oci tool installed and Oracle Cloud API configured 
+# https://docs.oracle.com/en-us/iaas/Content/API/Concepts/apisigningkey.htm#apisigningkey_topic_How_to_Generate_an_API_Signing_Key_Console
+# Author: Zhou Dai
+
+$startTime = Get-Date
+
+# save region names to list
+$regions = @('FRA','IAD','LHR','SIN','SYD','YUL','YYZ')	
+
+# construct query text
+$querytext="QUERY instance resources return region, timeMaintenanceRebootDue where timeMaintenanceRebootDue > 'Now' sorted by timeMaintenanceRebootDue desc" 
+
+""
+Write-Host "Start printing VMs maintenance reboot time for each region..."
+""
+foreach ($i in $regions)
+{
+    # call query on each region
+    write-host  "$i..."
+    oci search resource structured-search `
+    --query-text $querytext `
+    --query 'data.items[].{name:"display-name",state:"lifecycle-state",reboottime:"additional-details"."timeMaintenanceRebootDue", region:"additional-details"."region"}' `
+    --region $i `
+    | ConvertFrom-Json | format-table  
+    ""
+}
+
+Write-Host "Done!"
+
+$endTime = Get-Date
+$executionTime = $endTime - $startTime
+Write-Host "Script execution time: $executionTime"
+
+
+    
+
+    
+
+
+
