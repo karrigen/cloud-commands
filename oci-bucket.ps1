05170d4499c8fccec77339dd3912b839398b485d
karrigen <daizhou@hotmail.com>
2024-12-17 16:58:32 +1030

    Update oci-bucket.ps1

--- oci-bucket.ps1
+++ oci-bucket.ps1
@@ -1,16 +1,16 @@
 # list all buckets
 oci os bucket list `
-	--compartment-id "$compartment_id" `
-	--region 'syd' `
-	--query 'data[].name' | jq  -r '.[]' 
+    --compartment-id "$compartment_id" `
+    --region 'syd' `
+    --query 'data[].name' | jq  -r '.[]' 
 
 # list bucket content
 $bucket_name = "gcp-migration"
 oci os object list `
-	--region 'syd' `
-	--bucket-name $bucket_name `
-	--query 'data[].{name:name,size:size,tier:"storage-tier"}' `
-	| convertFrom-json | sort-object -property name | format-table
+    --region 'syd' `
+    --bucket-name $bucket_name `
+    --query 'data[].{name:name,size:size,tier:"storage-tier"}' `
+    | convertFrom-json | sort-object -property name | format-table
 
 # retrieve data
 Invoke-WebRequest -URI <URI LINK>
