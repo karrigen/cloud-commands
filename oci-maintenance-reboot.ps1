80b267aa6cd6d7c6d93dce45603da8dc1dc63711
karrigen <daizhou@hotmail.com>
2024-12-17 16:56:43 +1030

    Update oci-maintenance-reboot.ps1

--- oci-maintenance-reboot.ps1
+++ oci-maintenance-reboot.ps1
@@ -1,5 +1,5 @@
 oci search resource structured-search `
-	--query-text "QUERY instance resources return region, timeMaintenanceRebootDue where timeMaintenanceRebootDue > 'Now' sorted by timeMaintenanceRebootDue desc" `
-	--query 'data.items[].{name:"display-name",state:"lifecycle-state",RebootTime:"additional-details"."timeMaintenanceRebootDue", region:"additional-details"."region"}' `
-	--region 'syd' `
-	| convertFrom-json | sort-object -property name | format-table
+    --query-text "QUERY instance resources return region, timeMaintenanceRebootDue where timeMaintenanceRebootDue > 'Now' sorted by timeMaintenanceRebootDue desc" `
+    --query 'data.items[].{name:"display-name",state:"lifecycle-state",RebootTime:"additional-details"."timeMaintenanceRebootDue", region:"additional-details"."region"}' `
+    --region 'syd' `
+    | convertFrom-json | sort-object -property name | format-table
